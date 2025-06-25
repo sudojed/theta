42a14e1b44d6b89815e8f2d5f0341774582dd9e1
abnerLouren <abnerjaredejede@gmail.com>
2025-06-25 20:44:56 +0100

    definindo classe funcao

--- src/main/java/ao/sudojed/backend/recursos/funcao/Funcao.java
+++ src/main/java/ao/sudojed/backend/recursos/funcao/Funcao.java
@@ -2,7 +2,7 @@ package main.java.ao.sudojed.backend.recursos.funcao;
 
 import java.util.HashMap;
 
-public class Funcao {
+public abstract class Funcao {
     private String leiFormacao;
     private String dominio;
     private String zeros;
@@ -11,54 +11,64 @@ public class Funcao {
     private HashMap<String, String> monotonia;
     private HashMap<String, String> concavidade;
 
-    public void limitar(int tendencia){
+    public abstract void limitar(int tendencia);
+    public abstract void derivar();
+    public abstract void integrar();
 
-    }
-    
-    public void derivar(){
-
-    }
-
-    public void integrar(){
-        
-    }
-
-
+    // Getters and setters
     public String getDominio() {
         return dominio;
     }
+
     public void setDominio(String dominio) {
         this.dominio = dominio;
     }
+
     public String getZeros() {
         return zeros;
     }
+
     public void setZeros(String zeros) {
         this.zeros = zeros;
     }
+
     public String getParidade() {
         return paridade;
     }
+
     public void setParidade(String paridade) {
         this.paridade = paridade;
     }
+
     public String getVariacaoSinal() {
         return variacaoSinal;
     }
+
     public void setVariacaoSinal(String variacaoSinal) {
         this.variacaoSinal = variacaoSinal;
     }
+
     public HashMap<String, String> getMonotonia() {
         return monotonia;
     }
+
     public void setMonotonia(HashMap<String, String> monotonia) {
         this.monotonia = monotonia;
     }
+
     public HashMap<String, String> getConcavidade() {
         return concavidade;
     }
+
     public void setConcavidade(HashMap<String, String> concavidade) {
         this.concavidade = concavidade;
     }
-     
+
+    public String getLeiFormacao() {
+        return leiFormacao;
+    }
+
+    public void setLeiFormacao(String leiFormacao) {
+        this.leiFormacao = leiFormacao;
+    }
 }
