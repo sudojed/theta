4bf1c0d9208bd0cbf52b4389fc7d0671d7de5bbf
abnerLouren <abnerjaredejede@gmail.com>
2025-08-18 21:21:17 +0100

    Jul 18 09:21

--- src/main/java/ao/sudojed/backend/recursos/funcao/polinomial/Afim.java
+++ src/main/java/ao/sudojed/backend/recursos/funcao/polinomial/Afim.java
@@ -8,21 +8,25 @@ public class Afim extends FuncaoPolinomial {
     private Double coeficienteAngular;
     private Double coeficienteLinear;
     private String leiFormacao;
-    private String regex = "(^[+-]?\\d+)x(^(1))?([+-]?\\d+)?";
+    private String regex = "^([+-]?\\d+)x(\\^1)?([+-]?\\d+)?$";
     private String derivada;
     private String integral;
+
     public static void main(String[] args) {
-        System.out.println(new Afim("2x-7").derivar());
+        System.out.println(new Afim("2x-7").integrar());
     }
 
     public Afim(String expr) {
-        
         Pattern pattern = Pattern.compile(regex);
         this.leiFormacao = expr.replace("x", "x^1");
         Matcher matcher = pattern.matcher(leiFormacao);
+
         if (matcher.find()) {
             this.coeficienteAngular = Double.valueOf(matcher.group(1));
-            this.coeficienteLinear = Double.valueOf(matcher.group(4));
+            String b = matcher.group(3);
+            this.coeficienteLinear = (b != null) ? Double.valueOf(b) : 0.0;
+        } else {
+            throw new IllegalArgumentException("Expressão inválida: " + expr);
         }
     }
 
@@ -36,21 +40,22 @@ public class Afim extends FuncaoPolinomial {
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(this.leiFormacao);
         if (matcher.find()) {
-            this.derivada = matcher.group(2);
+            this.derivada = matcher.group(1);
         }
         return Double.parseDouble(derivada);
     }
-    
+
     @Override
-    public String integrar(){
+    public String integrar() {
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(this.leiFormacao);
         if (matcher.find()) {
-            this.integral = leiFormacao.replace("^1", "^2");
+            this.integral = leiFormacao.replace(matcher.group(2), "^2");
+            this.integral = leiFormacao.replace(matcher.group(3), matcher.group(3).concat("x"));
         }
         return this.integral;
     }
-    
+
     public void setCoeficienteLinear(Double coeficienteLinear) {
         this.coeficienteLinear = coeficienteLinear;
     }
@@ -58,7 +63,7 @@ public class Afim extends FuncaoPolinomial {
     public void setCoeficienteAngular(Double coeficienteAngular) {
         this.coeficienteAngular = coeficienteAngular;
     }
-    
+
     public Double getCoeficienteAngular() {
         return this.coeficienteAngular;
     }
